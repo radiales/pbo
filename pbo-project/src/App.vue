3dd3fc8537670245cb5fefb7a04335b414c5e4fe
silver1bear <juliusthefirst@gmail.com>
2019-10-29 15:00:18 +0100

    Landing page changes

--- pbo-project/src/App.vue
+++ pbo-project/src/App.vue
@@ -1,37 +1,51 @@
 <template>
-  <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-    <span id="headerSalad">🥗</span>
-    <HelloWorld id="helloDiv" msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<!--<img alt="Vue logo" src="./assets/logo.png">-->
+		<!--<span id="headerSalad">🥗</span>-->
+		<div id="appHeader">
+			<div id="centeredDiv">
+				<h2>MealShare</h2>
+				<h5>What do you want to eat today?</h5>
+			</div>
+		</div>
+		<Home id="homeDiv" msg="testMessage"/>
+	</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue"
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		HelloWorld,
+		Home
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 
 #hellDiv{
-  width: 100%;
+	width: 100%;
 }
 
-#headerSalad{
-  font-size: 48pt;
+#appHeader{
+	text-align: center;
+}
+
+#centeredDiv{
+	text-align: left;
+	width: 75%;
+	margin: 0 auto;
 }
 </style>
---
+++ pbo-project/src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+	<div class="landingPage">
+		<div>
+			<form class="tasteForm form">
+				<div class="tasteGroup">
+					<input type="radio" id="sweet" value="süß"/>
+					<label for="sweet">süß</label>
+				</div>
+				<div class="tasteGroup">
+					<input type="radio" id="herzhaft" value="herzhaft"/>
+					<label for="herzhaft">herzhaft</label>
+				</div>
+				<div class="tasteGroup">
+					<input type="radio" id="spicy" value="spicy"/>
+					<label for="spicy">spicy</label>
+				</div>
+			</form>
+			<form class="countForm form">
+				<label>Für wie viele Personen?</label><br>
+				<input type="number">
+			</form>
+			<div class="form">
+				<label>Zutaten</label>
+				<div class="ingridients chosenParent">
+					<span>Ausgewählt:</span>
+					<div class="chosen">
+						<span>Tomaten</span>
+						<span>Schinkenwürfel</span>
+					</div>
+				</div>
+				<div class="ingridients availableParent">
+					<span>Verfügbar</span>
+					<div class="available">
+						<span>Spinat</span>
+						<span>Gurken</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+.tasteGroup{
+	margin: 5px;
+	padding: 5px;
+	border: 1px  solid rgb(29, 86, 161);
+	border-radius: 10px;
+	background: rgb(29, 86, 161);
+	color: white;
+	display: inline;
+}
+
+.tasteForm > input{
+	background: black;	
+}
+
+.tasteForm input, .tasteForm label{
+	padding: 5px;
+}
+
+.countForm input{
+	border-radius: 15px; 
+}
+
+.form{
+	background: rgb(101, 142, 196);
+	padding: 20px;
+	margin-bottom: 15px;
+}
+
+.ingridients span{
+	color: grey;
+}
+
+.chosen span {
+	background: rgb(29, 86, 161);
+	color: white;
+}
+
+</style>
