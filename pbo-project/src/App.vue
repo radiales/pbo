95bc64c4d5e69af2e505c14c8fd8c53d7313eee2
Beleg <Nedselskiy@gmail.com>
2019-10-29 08:02:46 +0100

    -created Recipes.vue -designed and worked on separated areas -active and inactive buttons added -added overall background colors (beta)

--- pbo-project/src/App.vue
+++ pbo-project/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <span id="headerSalad">🥗</span>
-    <HelloWorld id="helloDiv" msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld id="helloDiv" msg="Welcome to Your Vue.js App"/>-->
+    <Recipes id="recipes" msg="This is recipes page"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Recipes from "./components/Recipes.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Recipes
   }
 }
 </script>
