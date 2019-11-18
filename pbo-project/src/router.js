2c19ddc3d9f1ae8187d88bad7f5d4b5005f2922a
silver1bear <juliusthefirst@gmail.com>
2019-11-18 20:32:57 +0100

    Started to add animated transitions of components by router.

--- pbo-project/src/router.js
+++ pbo-project/src/router.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './components/Home.vue'
-import Recipes from "./components/Recipes.vue"
-import Last from "./components/Last"
+import Vue      from 'vue'
+import Router   from 'vue-router'
+import Home     from './components/Home.vue'
+import Recipes  from "./components/Recipes.vue"
+import Last     from "./components/Last"
 
 Vue.use(Router)
 
 export default new Router({
+
+  routes: [
+    {
+      path: "/",
+      components: {
+        default:    Home,
+        recipeView: Recipes,
+        lastView:   Last
+      }
+    }
+  ]
+
+  /*
   routes: [
     {
       path: '/',
@@ -25,11 +38,12 @@ export default new Router({
     },
     {
       path: '/about',
-      name: 'about',
+      name: 'about',*/
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    //}
+  //]
 })
+
