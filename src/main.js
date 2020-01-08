41c980aad9c048d22478921a3aa0afef97a32b3a
Julius B <36518501+silver1bear@users.noreply.github.com>
2020-01-08 12:00:10 +0100

    Fixed routes for new structure

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import Vue from 'vue'
-
-//import BootstrapVue from "../node_modules/bootstrap-vue"
 import App from './App.vue'
-
 import router from './router'
-//import '../node_modules/bootstrap/dist/css/bootstrap.css'
-//import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
-
-//FIX
 import Selectable from "./components/Selectable"
 import Constraint from "./components/Constraint"
 
 Vue.config.productionTip = false
 
-//Vue.use(BootstrapVue);
 Vue.component("Home");
 Vue.component("Recipes");
 Vue.component("Last");
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue        from 'vue'
 import Router     from 'vue-router'
-import Home       from './components/Home.vue'
-import Recipes    from "./components/Recipes.vue"
-import Last       from "./components/Last"
+import Home       from './views/Home.vue'
+import Recipes    from "./views/Recipes.vue"
+import Last       from "./views/Last"
 
 Vue.use(Router)
 
@@ -31,32 +31,5 @@ export default new Router({
       }
     }
   ]
-
-  /*
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/recipes',
-      name: 'recipes',
-      component: Recipes
-    },
-    {
-      path: '/last',
-      name: 'last',
-      component: Last
-    },
-    {
-      path: '/about',
-      name: 'about',*/
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    //}
-  //]
 })
 
